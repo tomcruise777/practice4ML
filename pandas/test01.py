fe47358b26bd6994dc7412b3587e2308de0aa77b
zhaoxin <zhaoxin@zhangyue.com>
2018-03-19 17:26:46 +0800

    add pandas

---
+++ pandas/test01.py
@@ -0,0 +1,19 @@
+#! -*- coding: UTF-8 -*-
+#
+#   @author zhaoxin
+#   @date 2018-03-19
+#
+
+
+import tensorflow as tf
+import pandas as pd
+
+
+def main():
+
+    print("succ import!")
+
+
+if __name__ == '__main__':
+
+    main()
