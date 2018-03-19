9f5cb5e0987013eb035940d203041a9b936dd80b
zhaoxin <zhaoxin@zhangyue.com>
2018-03-19 18:11:44 +0800

    add ipynb

--- pandas/test01.py
+++ pandas/test01.py
@@ -7,11 +7,26 @@
 
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+
+
+def basic_op():
+    city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+    population = pd.Series([852469, 1015785, 485199])
+    pd.DataFrame({ 'City name': city_names, 'Population': population })
+
+def read_csv():
+    print("将整个文件加载到 DataFrame")
+    california_housing_dataframe = pd.read_csv("california_housing_train.csv", sep=",")
+    california_housing_dataframe.describe()
 
 
 def main():
 
-    print("succ import!")
+    print("BEGIN")
+    basic_op()
+
+    print("END")
 
 
 if __name__ == '__main__':
